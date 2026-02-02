1713ea71b0c0bcee25be6d0dc7bbff84afc8f7d1
krishnagupta031003-ops <krishnagupta031003>
2026-02-02 20:52:02 +0530

    start again

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,9 +1,14 @@
-const router = require("express").Router()
-const {womenkurtaset,womensaree,womenkurties, womendresses}= require("../controllers/category.controller")
+const router = require("express").Router();
+const {
+  womenkurtaset,
+  womensaree,
+  womenkurties,
+  womendresses,
+} = require("../controllers/category.controller");
 
-router.get("/womenkurtaset",womenkurtaset)
-router.get("/womensaree",womensaree)
-router.get("/womenkurties",womenkurties)
-router.get("/womendresses",womendresses)
+router.get("/womenkurtaset", womenkurtaset);
+router.get("/womensaree", womensaree);
+router.get("/womenkurties", womenkurties);
+router.get("/womendresses", womendresses);
 
-module.exports = router
+module.exports = router;
